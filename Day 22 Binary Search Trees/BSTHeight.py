99ab9a333785224a7e0eef7f50cef56a77010cea
chien-wei <chien-wei@outlook.com>
2018-01-27 21:57:31 -0700

    add day22

--- Day 22 Binary Search Trees/BSTHeight.py
+++ Day 22 Binary Search Trees/BSTHeight.py
@@ -15,22 +15,21 @@ class Solution:
                 root.right=cur
         return root
 
-class Node:
-    def __init__(self,data):
-        self.right=self.left=None
-        self.data = data
-class Solution:
-    def insert(self,root,data):
-        if root==None:
-            return Node(data)
-        else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
-            else:
-                cur=self.insert(root.right,data)
-                root.right=cur
-        return root
+    def getHeight(self,root):
+        
+        def getHeightHelper(node, level):
+            if not node:
+                return level - 1
+            result = level
+            if node.right:
+                result = max(result, getHeightHelper(node.right, level+1))
+            if node.left:
+                result = max(result, getHeightHelper(node.left, level+1))
+            return result
+        
+        if not root:
+            return 0
+        return getHeightHelper(root, 0)
 
 
 T=int(input())
@@ -40,4 +39,4 @@ for i in range(T):
     data=int(input())
     root=myTree.insert(root,data)
 height=myTree.getHeight(root)
-print(height)
+print(height) 
