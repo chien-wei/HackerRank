869bc0a32b5574b6847a4642d848e1d2325dc0e2
Chien-Wei <chien-wei@outlook.com>
2018-01-31 15:23:37 -0700

    add day27

---
+++ Day 27 Testing/map.py
@@ -0,0 +1,10 @@
+def showTest(n, k, a):
+    print(str(n) + " " + str(k))
+    print(" ".join(map(str, a)))
+
+print('5')
+showTest(4, 3, [-1, -3, 4, 2])
+showTest(5, 2, [0, -1, 2, 1, 4])
+showTest(4, 3, [-1, -3, 4, 2])
+showTest(5, 2, [0, -1, 2, 1, 4])
+showTest(4, 3, [-1, -3, 4, 2])
